333c000ff014f41b2d32d39849ef6df35295fce7
YJP-0104 <patel.yashjat@northeastern.edu>
2024-10-06 00:47:52 -0400

    Assingment 3

---
+++ Lab3/assingment_3/src/model/Patient.java
@@ -0,0 +1,88 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author yashpatel
+ */
+public class Patient {
+
+    private String name;
+    private String lname;
+    private String email;
+    private String gender;
+    private String paitientType;
+    private String age;
+    private String message;
+    private ImageIcon profilePicture;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getPaitientType() {
+        return paitientType;
+    }
+
+    public void setPaitientType(String paitientType) {
+        this.paitientType = paitientType;
+    }
+
+    public ImageIcon getProfilePicture() {
+        return profilePicture;
+    }
+
+    public void setProfilePicture(ImageIcon profilePicture) {
+        this.profilePicture = profilePicture;
+    }
+
+}
